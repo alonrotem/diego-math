6299c2a0b522f645e273891516a29cdd1a4f3f49
Alon Rotem <Alon.Rotem@cssregtech.com>
2021-02-09 20:03:31 +0100

    Temporary fix

--- scripts/diego-math.js
+++ scripts/diego-math.js
@@ -3,11 +3,11 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
 
-var floor = 320;
+var floor = 448;
 
 const keysdown = [];
 const platforms = [];
-
+/*
 function Character(sprite, x, y, width, height, directionSpiteCoords)
 {
     this.sprite = new Image();
@@ -18,31 +18,50 @@ function Character(sprite, x, y, width, height, directionSpiteCoords)
     this.width = width;
     this.height = height;
     this.directionSpiteCoords = directionSpiteCoords;
-    this.speed = 3;
+    this.speed = 2;
     this.currentSpriteMoveCoords = 1;
     this.jumpHeight = 170;
-    this.jumpSpeed = 4;
-    this.gravity = (this.jumpSpeed / this.jumpHeight) * 2;
+    this.jumpSpeed = 3;
+    this.gravity = 0;//(this.jumpSpeed / this.jumpHeight) * 2;
     //don't change the running frame change too quickly
     this.jumpDirection = "";
     this.framesHold = 6;
     var curFrame = 0;
+    this.walkspeed = 10;
 
-    this.draw = function() {
-        $("#info1").text("this.baseY=" + this.baseY);
-        $("#info2").text("this.y=" + this.y);
+    this.draw = function(interval) {
+        var framespersec = 60;
+        var pxperframejump = 8;
+        var pxperframewalk = 8;
+
+        this.jumpSpeed = (interval / 1000) * (framespersec * pxperframejump);
+        this.walkspeed = (interval / 1000) * (framespersec * pxperframewalk);
+        //console.log ("jumpspeed " + this.jumpSpeed);
+        //4 px per frame
+        //60 frames per second
+        //240 px per second
+        //example: elaped interval 30ms
+        //240 px -> 1000s
+        //7.2    -> 30ms
+        //this.jumpSpeed = 60*60/1000 
+        //---
+        //gravity start with 0
+        //each iteration: gravity += 10; 
+
+        $("#info1").text("this.y=" + this.y);
+        $("#info2").text("this.jumpSpeed=" + this.jumpSpeed);
         var spriteCoordX = 0;
         var spriteCoordY = 0;
         var direction = "X";
         if(keysdown["ArrowRight"] && this.x < 800-this.width)
         {
-            this.x += this.speed;
+            this.x += this.walkspeed;
             direction = "R";
 
         }
         else if(keysdown["ArrowLeft"] && this.x > 0)
         {
-            this.x -= this.speed;
+            this.x -= this.walkspeed;
             direction = "L";          
         }
         //jumping
@@ -53,24 +72,37 @@ function Character(sprite, x, y, width, height, directionSpiteCoords)
             if(!this.jumpDirection)
             {
                 this.jumpDirection = "U";
+                this.gravity = 0;
             }
             //going up
             if(this.jumpDirection == "U")
             {
                 if(this.y > this.baseY - this.jumpHeight)
                 {
-                    $("#info3").text("jumping up to " + (this.baseY - this.jumpHeight));
-                    console.log("jumping up")
+                    $("#info3").text("gravity " + this.gravity);
+                    console.log("jumping. gravity=" + this.gravity);
+                    //console.log("jumping up")
                     direction = "J";
-                    this.y -= this.jumpSpeed;
-                    this.jumpSpeed -= this.gravity;
+                    
+                    this.y -= (this.jumpSpeed - this.gravity);
+                    if((this.jumpSpeed - this.gravity) <= 0){
+                        this.jumpDirection = "D";
+                        $("#info3").text("max height");
+                        console.log("max height")
+                        this.jumpDirection = "D";
+                        this.gravity = 0;                        
+                    }
+                    else
+                        this.gravity += (this.jumpSpeed/100);
+                    //this.jumpSpeed -= this.gravity;
                 }
                 else
                 {
                     $("#info3").text("max height");
                     console.log("max height")
                     this.jumpDirection = "D";
-                    this.jumpSpeed = 4;
+                    this.gravity = 0;
+                    //this.jumpSpeed = 4;
                 }
             }
             /*
@@ -104,7 +136,7 @@ function Character(sprite, x, y, width, height, directionSpiteCoords)
                     this.jumpSpeed = 4;
                     this.jumpDirection = "";
                 }                
-            }*/
+            }*//*
         }
 
         if(this.jumpDirection != "U")
@@ -130,11 +162,12 @@ function Character(sprite, x, y, width, height, directionSpiteCoords)
             {
                 //if()
                 {
-                    $("#info3").text("falling");
-                    //console.log("not on platform. falling.");
+                    $("#info3").text("this.gravity=" + this.gravity);
+                    console.log("falling. gravity=" + this.gravity);
                     direction = "J";
-                    this.y += this.jumpSpeed;
-                    this.jumpSpeed += this.gravity;
+                    this.y += this.jumpSpeed + this.gravity;
+                    //this.jumpSpeed += this.gravity;
+                    this.gravity += (this.jumpSpeed/100);
                 }
             }
             else
@@ -144,10 +177,10 @@ function Character(sprite, x, y, width, height, directionSpiteCoords)
                 $("#info3").text("on platform");
                 //console.log("hit a platform");
                 this.jumpDirection = "";
-                direction = "X";
+                direction = (direction != "R" && direction != "L")?  "X" : direction;
                 this.y = platforms[onPlatform].y - this.height;
                 this.baseY = platforms[onPlatform].y - this.height;
-                this.jumpSpeed = 4;
+                //this.jumpSpeed = 4;
             }
             else 
             {
@@ -156,10 +189,10 @@ function Character(sprite, x, y, width, height, directionSpiteCoords)
                     $("#info3").text("on floor");
                 //console.log("hit the floor");
                 this.jumpDirection = "";
-                direction = "X";
+                direction = (direction != "R" && direction != "L")?  "X" : direction;
                 this.y = floor;
                 this.baseY = floor;
-                this.jumpSpeed = 4;
+                //this.jumpSpeed = 4;
                 }
             }
             }
@@ -199,7 +232,7 @@ function Character(sprite, x, y, width, height, directionSpiteCoords)
         }
         ctx.drawImage(this.sprite, spriteCoordX, spriteCoordY, this.width, this.height, this.x, this.y, this.width, this.height);
     }
-}
+}*/
 
 function Platform(x, y, width)
 {
@@ -227,7 +260,7 @@ function Platform(x, y, width)
     this.isOnPlatform = function (character) {
         if(
             (character.x + character.width > this.x && character.x < this.x + this.width) &&
-            (character.y + character.height > this.y - this.heightThreshold && character.y + character.height <= this.y)
+            (character.y + character.height >= this.y && character.y + character.height <= this.y+this.heightThreshold)
             )
             /*
         if((character.x + character.width > this.x || 
@@ -246,7 +279,7 @@ function Platform(x, y, width)
 
 const background = new Image();
 background.src = "images/background/backgroundColorGrass-scaled.png";
-
+/*
 const diego = new Character("images/sprites/diego.png", canvas.width / 2 - 48, floor, 96, 128,
     {
         "X": [0,0],
@@ -254,28 +287,188 @@ const diego = new Character("images/sprites/diego.png", canvas.width / 2 - 48, f
         "L": [[2, 5], [1, 5], [0, 5]],
         "J": [4, 0]
     }
-);
+);*/
 const platformsWidth = 100;
 const platform1 = new Platform(90, 300, platformsWidth);
 const platform2 = new Platform(canvas.width-90-platformsWidth, 300, platformsWidth);
 const platform3 = new Platform(canvas.width/2 - platformsWidth /2, 200, platformsWidth);
 
-//var fps, fpsInterval, startTime, now, then, elapsed;
+//--------------------------------
+// define the player.
+// update() updates position and response to keyboard
+// draw() draws the player
+// start() sets start position and state
+const player = function(sprite, x, y, width, height, directionSpiteCoords) {
+    this.sprite = new Image();
+    this.sprite.src = sprite;
+    this.imageloaded = false;
+    var that = this;
+    this.sprite.onload = function(){ 
+        that.imageloaded = true; 
+    };
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.dx = 0; // delta x and y
+    this.dy = 0;
+    //this.size = 20;
+    this.color = 'lime';
+    this.onGround = false;
+    this.jumpPower = -10;  // power of jump smaller jumps higher eg -10 smaller than -5
+    this.moveSpeed = 6;
+    this.spriteCoordX = 0;
+    this.spriteCoordY = 0;
+    this.directionSpiteCoords = directionSpiteCoords;
+    this.direction = "";
+    this.onPlatform = -1;
+    this.update = function() {
+        this.onPlatform = -1;
+        for(var i=0; i < platforms.length; i++)
+        {
+            //console.log("checking platform " + i);
+            if(platforms[i].isOnPlatform(this))
+            {
+                this.onPlatform = i;
+                
+                this.y = platforms[i].y - this.height;
+                //this.onGround = true;
+                //console.log("On platform " + onPlatform);
+                break;
+            }
+        }        
+      // react to keyboard state
+      if ((keysdown[" "] || keysdown["ArrowUp"]) && (this.onGround || this.onPlatform != -1)) { this.dy = this.jumpPower;
+        this.direction = "J";
+        $("#info2").text("Direction " + this.direction);
+        $("#info1").text("Jumping");
+        //this.spriteCoordX = this.width * this.directionSpiteCoords["J"][0];
+        //this.spriteCoordY = this.height * this.directionSpiteCoords["J"][1];   
+     }
+      if (keysdown["ArrowLeft"]) { this.dx = -this.moveSpeed;
+        this.direction = "L";
+        $("#info1").text("Left");
+        $("#info2").text("Direction " + this.direction);
+        //this.spriteCoordX = this.width * this.directionSpiteCoords["L"][0][0];
+        //this.spriteCoordY = this.height * this.directionSpiteCoords["L"][0][1];
+     }
+      if (keysdown["ArrowRight"]) { this.dx = this.moveSpeed;
+        this.direction = "R";
+        $("#info1").text("Right");
+        $("#info2").text("Direction " + this.direction);
+        //this.spriteCoordX = this.width * this.directionSpiteCoords["R"][0][0];
+        //this.spriteCoordY = this.height * this.directionSpiteCoords["R"][0][1];        
+     }
+   
+      // apply gravity drag and move player
+
+      $("#info1").text((this.onPlatform != -1)? "!!On platform " + this.onPlatform: "Falling");
+
+      this.dy += world.gravity;
+      this.dy *= world.drag;
+      this.dx *= this.onGround || this.onPlatform != -1 ? world.groundDrag : world.drag;
+      this.x += this.dx;
+      this.y += this.dy;
+
+      //THIS MAY BE THE PROBLEM
+      // test ground contact and left and right limits
+      if ((this.y + this.height >= world.ground) || (this.onPlatform != -1 /*&& this.direction != "J"*/)) {
+        this.y = (this.onPlatform == -1)? world.ground - this.height : platforms[this.onPlatform].y - this.height;
+        //if(this.direction !="J")
+         //   this.dy = 0;
+        this.onGround = true;
+        $("#info1").text((this.onPlatform == -1)? "on ground" : "on platform!! " + this.onPlatform);
+        
+        if(!this.direction)
+        {
+           this.direction = "X";
+           $("#info2").text("Direction " + this.direction);
+        }
+        
+      } else {
+        this.onGround = false;
+        if(this.direction == ""){
+            this.direction = "J";
+            $("#info2").text("Direction " + this.direction);
+        }
+            $("#info1").text((this.onPlatform != -1)? "~~On platform " + this.onPlatform: "~~still jumping");
+      }
+      if (this.x > ctx.canvas.width) {
+        this.x -= ctx.canvas.width;
+      } else if (this.x + this.width < 0) {
+        this.x += ctx.canvas.width;
+      }
+    };
+    this.draw = function(){
+      //drawRect(this.x, this.y, this.size, this.size, this.color);
+      console.log("this.direction=" + this.direction);
+      console.log(this.direction + " : " + this.directionSpiteCoords[this.direction][0][0]);
+      console.log(this.direction + " : " + this.directionSpiteCoords[this.direction][0][1]);
+      this.spriteCoordX = this.width * this.directionSpiteCoords[this.direction][0][0];
+      this.spriteCoordY = this.height * this.directionSpiteCoords[this.direction][0][1];         
+      if(this.imageloaded)
+        ctx.drawImage(this.sprite, this.spriteCoordX, this.spriteCoordY, this.width, this.height, this.x, this.y, this.width, this.height);
+      this.direction = "";
+    };
+    this.start = function (){
+      this.x = ctx.canvas.width / 2 - this.width / 2;
+      this.y = world.ground - this.height;
+      this.onGround = true;
+      this.dx = 0;
+      this.dy = 0;
+    }
+  }
+  // define world
+  const world = {
+    gravity: 0.3, // strength per frame of gravity
+    drag: 0.999, // play with this value to change drag
+    groundDrag: 0.8,//0.9, // play with this value to change ground movement
+    ground: floor
+  }
+
+  // From OP
+function drawRect(x, y, width, height, color) {
+    ctx.beginPath();
+    ctx.rect(x, y, width, height);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+  }
+//--------------------------------
+
+
+var fps, fpsInterval, startTime, now, then, elapsed;
 
 function startAnimating(fps)
 {
     fpsInterval = 1000/fps;
-    //then = Date.now();
-    //startTime = then;
+    then = Date.now();
+    startTime = then;
     animate(); 
 }
+
+/*
+requestanimation frame
+measure how much time from the previous frame
+move speed according to px/time <- convert to px/required time
+*/
+p = new player("images/sprites/diego.png", canvas.width / 2 - 48, floor, 96, 128,
+{
+    "X": [[0,0]],
+    "R": [[6,2], [7, 2], [8,2]],
+    "L": [[2, 5], [1, 5], [0, 5]],
+    "J": [[4, 0]]
+}
+);;
+p.start();
 
 function animate(fps)
 {
     //requestAnimationFrame(animate);
-    setInterval(function(){
-        //now = Date.now();
-        //elapsed = now - then;
+    //setInterval(function(){
+        now = Date.now();
+        elapsed = now - then;
+        then = now;
         //if(elapsed > fpsInterval)
         {
         //    then = now - (elapsed % fpsInterval);
@@ -294,11 +487,14 @@ function animate(fps)
             {
                 platforms[i].draw();
             }
-            diego.draw();
+            //diego.draw(elapsed);
+            p.update();
+            p.draw();            
         }    
-    },1000/fps);
+    //},1000/fps);
 
-    //requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
+    //keysdown[' '] = true;
 }
 animate(155);
 
